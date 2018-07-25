46d50464ea0932baaf712844683143436d2c0084
Cameron Bjorklund <cameron.bjorklund@singlewire.com>
2018-07-25 10:58:56 -0500

    Adding doc strings to roll functions

--- src/rpg_action/dice.clj
+++ src/rpg_action/dice.clj
@@ -1,10 +1,14 @@
 (ns rpg-action.dice)
 
 (defn roll
+  "Executes a dice roll. The kind of dice being rolled is the max"
   [max]
   (+ (rand-int max) 1))
 
 (defn process-roll
+  "Processes a roll with options.
+  Max is required. It is the type of dice being rolled.
+  The explode? option will re-roll dice that = max and return a list of rolls"
   ([max]
    (process-roll max false))
   ([max explode?]
