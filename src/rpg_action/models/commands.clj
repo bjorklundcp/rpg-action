f8feeb92743e9a5f55021dd8ed84b5494b275b57
Charlie Ciccia <charlie.ciccia@singlewire.com>
2018-08-02 12:18:51 -0500

    help clarification

--- src/rpg_action/models/commands.clj
+++ src/rpg_action/models/commands.clj
@@ -1,13 +1,13 @@
 (ns rpg-action.models.commands
   (:require [clojure.spec.alpha :as s]))
 
-(def commands #{:help})
-(defn command?
-  [cmd]
-  (contains? commands cmd))
+(def help-options #{:help})
+(defn help-option?
+  [option]
+  (contains? help-options (keyword option)))
 
 (defmulti command first)
 (defmethod command :help [_]
-  (s/cat :type #{:help} :command (s/? command?)))
+  (s/cat :type #{:help} :help-option (s/? help-option?)))
 
 (s/def ::command (s/multi-spec command (fn [genv _tag] genv)))
